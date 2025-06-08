3b8f7a73d43bb0306010e4f1ca494941b4bc3ad5
nick <nikolay.bespalov.ext@deufol.com>
2025-06-08 14:14:07 +0500

    Update ESLint config to include ignores for build outputs and adjust rules; use `const` in `de-serialize.ts` for clarity.

--- src/compression/de-serialize.ts
+++ src/compression/de-serialize.ts
@@ -16,7 +16,7 @@ export function deserialize(serializedStr: string): number[] {
   const decodedStr = atob(serializedStr);
   const deltas = Array.from(decodedStr).map((ch) => ch.charCodeAt(0) - 33);
 
-  let numbers: number[] = [deltas[0]];
+  const numbers: number[] = [deltas[0]];
   for (let i = 1; i < deltas.length; i++) {
     numbers.push(numbers[i - 1] + deltas[i]);
   }
