752d37e6be17c88ea6b9f6ff0b70ab0805cdec69
nick <nikolay.bespalov.ext@deufol.com>
2025-06-08 14:46:09 +0500

    Update Jest config to enable TypeScript preset and coverage reporting; fix badges in README.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,10 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
   transform: {
     ...tsJestTransformCfg,
   },
